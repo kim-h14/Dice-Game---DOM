f278eef5248a6da889a19b577be08c4d8bcebb9f
kim-h14 <kim.heuacker@gmail.com>
2023-12-30 06:33:30 +0100

    fonction for round adds score based on dice image and scores goes to 0 if dice is 1

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 // Add Event listener to button Roll Dice
 
-document.querySelector("button").addEventListener("click", rollDiceClick);
-
-// Random number created gives dice image value linked to event listener 
+document.querySelector("button.roll").addEventListener("click", rollDiceClick);
 function rollDiceClick() {
-  let randomNumber = Math.floor(Math.random()*6) + 1;
-  let image = "images/dice-" + randomNumber + ".png";
+  
+  // Random number created gives dice image value linked to event listener 
+  let diceValue = Math.floor(Math.random()*6) + 1;
+  let image = "images/dice-" + diceValue + ".png";
 
   document.querySelector(".dice .diceImg").setAttribute("src",image);
-  // Random number adds value to current round except if number = 1 then currentround goes back to 0
-  let currentRound = randomNumber;
-  
-  if (currentRound === 1) {
-    document.querySelector(".roundP2").innerHTML = "0";
+
+  // Get the current round value from the HTML and convert it to a number
+  let currentRound = parseInt(document.querySelector(".roundP2").innerHTML);
+
+  if (isNaN(currentRound)) {
+    currentRound = 0;
+  }
+
+  if (diceValue === 1) {
+    currentRound = 0;
   }
   else {
-    let currentRound = randomNumber;
-    document.querySelector(".roundP2").innerHTML = currentRound;
+    currentRound += diceValue;
   }
+  // Update the HTML with the new value for current round
+  document.querySelector(".roundP2").innerHTML = currentRound;
 }
+
+
+// Add Event listener to button Hold
+
+// document.querySelector("button.hold").addEventListener("click", holdScoreClick);
+
+// function linked to event listener to hold score
+function holdScoreClick() {
+
+}
+
+// Add Add Event listener to button New Game
+
+// document.querySelector("button.game").addEventListener("click", newGame);
+
+// function linked to event listener to start new game
+function newGame() {
+
+}
+
+
+// Winner loop 
+
+// for (const i = 0; querySelector("globalP1"))
